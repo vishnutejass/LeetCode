e0a670b0ef5d7a31400bb6a8926c3b0d22fa2430
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2023-08-31 16:35:06 +0530

    Time: 1832 ms (35.69%), Space: 0B (100.00%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -9,5 +9,5 @@
 
 SELECT project_id, ROUND(SUM(e.experience_years)/COUNT(e.experience_years), 2) as average_years
 FROM Project as p
-left JOIN Employee e ON p.employee_id = e.employee_id
+inner JOIN Employee e ON p.employee_id = e.employee_id
 GROUP BY project_id
