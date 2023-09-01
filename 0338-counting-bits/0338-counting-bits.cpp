a01d74770f6bbb22b5631d95178b73d0f3f10758
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2023-09-02 00:14:24 +0530

    Time: 6 ms (56.68%), Space: 7.9 MB (51.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,30 +1,59 @@
-
-#include <unordered_map>
-#include <cmath>
-#include <vector>
-
+#include<vector>
 class Solution {
-public:
-  int check(int n) {
-    int cnt = 0;
-    while (n != 0) {
-        n &= (n - 1);  
-        cnt++;
-    }
-    return cnt;
+     public:
+std::vector<int> countBits(int n) {
+    std::vector<int> arr(n + 1,0);
+ for(int i=0;i<=n;i++)
+         {   
+     arr[i]=arr[i/2]+i%2;
+ }
+
+  return arr;  
 }
+};
+// #include<vector>
+// class Solution {
+//     public:
+//     std::vector<int> countBits(int n) {
+//         std::vector<int> arr(n + 1,0);
+//         for(int i=0;i<=n;i++)
+//         {
+//             if(i%2==0){
+//                 arr[i]=arr[i/2];//because if we want to find for 2n we can just find for n ,as it will be just a left shift between n and 2n
+//             }
+//             else
+//                 arr[i]=arr[i/2]+1;
+//         }
+        
+//         return arr;
+//     }
+// };
+// #include <unordered_map>
+// #include <cmath>
+// #include <vector>
+
+// class Solution {
+// public:
+//   int check(int n) {
+//     int cnt = 0;
+//     while (n != 0) {
+//         n &= (n - 1);  
+//         cnt++;
+//     }
+//     return cnt;
+// }
 
     
-    std::vector<int> countBits(int n) {
-        std::vector<int> arr(n + 1);
-        std::vector<int> a;
-        for(int i = 0; i <= n; i++) {
-            arr[i] = check(i);
-            a.push_back(arr[i]);
-        }
-        return a;
-    }
-};
+//     std::vector<int> countBits(int n) {
+//         std::vector<int> arr(n + 1);
+//         std::vector<int> a;
+//         for(int i = 0; i <= n; i++) {
+//             arr[i] = check(i);
+//             a.push_back(arr[i]);
+//         }
+//         return a;
+//     }
+// };
 
 
 
