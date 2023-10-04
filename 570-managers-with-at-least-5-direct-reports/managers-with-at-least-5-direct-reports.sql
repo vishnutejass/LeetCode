1bca2bd51afcc7b8c95ded653c5f179377cea37a
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2023-10-04 16:37:13 +0530

    Time: 461 ms (98.44%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,20 +1,20 @@
-# Write your MySQL query statement below
-# select name
-# from Employee  e
-# where id= 
-# case when managerId is null then 0 else sum(managerId)/5 END
-# group by id,name
 
-   with cte as(
-    SELECT managerId
-    FROM Employee
-    GROUP BY managerId
-    HAVING COUNT(id) >= 5  #to filter aggregate function we use having clause
-   )
-
-  #  select name from Employee where id in (select managerId from cte)
-   select e.name 
-   from Employee e join
-   cte on
-   e.Id = cte.managerId
+#    with cte as(
+#     SELECT managerId
+#     FROM Employee
+#     GROUP BY managerId
+#     HAVING COUNT(id) >= 5  #to filter aggregate function we use having clause
+#    )
+#  select e.name 
+#    from Employee e join
+#    cte on
+#    e.Id = cte.managerId    
+ #or #  select name from Employee where id in (select managerId from cte)
    
+   # Write your MySQL query statement below
+select name from Employee where id in (select managerId from Employee group by managerId having count(*)>=5)
+
+# select name from Employee where id in(select managerId from Employee group by 
+# managerId having count(*)>=5)
+
+##checking whether that managerid is repeating or >5 timesor not 
