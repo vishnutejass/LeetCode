c29dbab9014e6eb6aeed27695259410edb75f91b
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2024-02-19 08:23:03 +0530

    Time: 700 ms (73.66%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,2 @@
-SELECT MAX(freq.num) AS num
-FROM (
-    SELECT num, COUNT(num) AS num_count
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-) AS freq;
+SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1) AS n;
+
