3e557c23ecdd4b69451e493527ed22de2b5f69fc
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2024-07-12 23:35:52 +0530

    Time: 281 ms (95.87%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,10 +1,26 @@
-# Write your MySQL query statement below
-with cte as(
-select employee_id,manager_id from Employees
-where salary < 30000
-)
-select employee_id from cte
-where manager_id  not in (
-    select employee_id from employees
-)
-order by employee_id
+-- # Write your MySQL query statement below
+-- with cte as(
+-- select employee_id,manager_id from Employees
+-- where salary < 30000
+-- )
+-- -- select employee_id from cte
+-- -- where manager_id  not in (
+-- --     select employee_id from employees
+-- -- )
+-- -- order by employee_id
+
+-- select employee_id from
+-- employees a where salary >30000
+-- AND manager_id IS NOT NULL
+-- and not exists(
+--     select 1 from employees b
+--     where  b.manager_id = a.employee_id
+-- )
+-- order by employee_id;
+
+SELECT employee_id
+FROM Employees a
+WHERE salary < 30000 
+      AND manager_id IS NOT NULL
+      AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
