0006cd4ce53771597afafba747f57036338c340e
Vishnu tejas <99108179+vishnutejass@users.noreply.github.com>
2024-07-26 06:40:43 +0530

    Time: 54 ms (37.67%) | Memory: 48.6 MB (70.56%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+  
+    return function() {
+        return n++;
+     
+    };
+};
+
+// class Counter {
+//     constructor(n){
+//         this.n = n;
+//     }
+//     increment(){
+//         return ++this.n;
+//     }
+// }
+// const counter = Counter(10);
+// counter.increment()
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
